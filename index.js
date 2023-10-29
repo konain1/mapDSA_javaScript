4528e8269b8bb9c75af158e2825d0b6e8f8cd722
Konain1 <kaunainkasab@gmail.com>
2023-10-29 11:47:15 +0530

    Set DSA

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 
-let data = new Map([
-    ['name','konain'],
-    ['add','kishanganj']
-])
+// let data = new Map([
+//     ['name','konain'],
+//     ['add','kishanganj']
+// ])
 
-data.set('school','insan school')
+// data.set('school','insan school')
 
-// data.forEach((item)=>{
+// // data.forEach((item)=>{
+// //     console.log(item)
+// // })
+
+
+// for (const key of data) {
+//     console.log(key[1])
+// }
+// data.delete('school')
+
+// console.log(data)
+
+const arr = [1,2,3,3,4,4,5,5,6]
+
+const mySet = new Set(arr)
+
+mySet.add(7)
+mySet.delete(3)
+
+// mySet.forEach((item)=>{
 //     console.log(item)
 // })
 
+console.log(mySet.size)
 
-for (const key of data) {
-    console.log(key[1])
-}
-data.delete('school')
-
-console.log(data)
